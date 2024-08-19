40c30b862122f0271c382115c7da16208eef906a
Kristof Neiyrnck <k.neirynck@gmail.com>
2024-08-20 00:53:18 +0200

    Add parameter to details route

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo"
         alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{housingLocation.name}}</h2>
       <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+      <p><a [routerLink]="['/details', housingLocation.id]">Learn more</a></p>
     </section>
   `,
   styleUrl: './housing-location.component.css'
