a746c5105693f4961f6f5d5dbb86747d82c07776
Kristof Neiyrnck <k.neirynck@gmail.com>
2024-08-19 21:23:10 +0200

    Add form to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section clas="results">
+      results
+    </section>
   `,
   styleUrl: './home.component.css'
 })
