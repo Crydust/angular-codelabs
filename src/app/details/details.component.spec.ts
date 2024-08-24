3e07477ba0f5953a776b05910a90f2bb41fa6db8
Kristof Neiyrnck <k.neirynck@gmail.com>
2024-08-24 14:10:39 +0200

    Fis tests

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsComponent } from './details.component';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
@@ -8,9 +10,22 @@ describe('DetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DetailsComponent]
+      imports: [DetailsComponent],
+      providers:
+        [
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              snapshot: {
+                params: of(convertToParamMap({
+                  id: '42'
+                }))
+              }
+            }
+          }
+        ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
--- src/app/housing-location/housing-location.component.spec.ts
+++ src/app/housing-location/housing-location.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HousingLocationComponent } from './housing-location.component';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
 
 describe('HousingLocationComponent', () => {
   let component: HousingLocationComponent;
@@ -8,12 +10,29 @@ describe('HousingLocationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HousingLocationComponent]
+      imports: [HousingLocationComponent],
+      providers:
+      [
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HousingLocationComponent);
     component = fixture.componentInstance;
+    component.housingLocation = {
+      id: 1,
+      name: "name",
+      city: "city",
+      state: "state",
+      photo: "photo.jpg",
+      availableUnits: 2,
+      wifi: false,
+      laundry: true
+    };
     fixture.detectChanges();
   });
 
